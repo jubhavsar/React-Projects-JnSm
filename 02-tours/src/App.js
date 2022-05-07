6733d7571ea71359fc5415cc0b3c61afece22b1c
jubhavsar <juhi_bhavsar@yahoo.com>
2022-05-07 09:26:23 -0400

    modify fetchTours function using try and catch

--- 02-tours/src/App.js
+++ 02-tours/src/App.js
@@ -1,36 +1,45 @@
-import React, { useState, useEffect } from 'react'
-import Loading from './Loading'
-import Tours from './Tours'
+import React, { useState, useEffect } from "react";
+import Loading from "./Loading";
+import Tours from "./Tours";
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-tours-project'
+const url = "https://course-api.com/react-tours-project";
 function App() {
- const [loading, setLoading] = useState(true);
- const [tours, setTours] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
 
- const fetchTours = async () => {
-  setLoading(true);
-  const response = await fetch(url);
-  const tours = await response.json();
+  //fetch Api data
+  const fetchTours = async () => {
+    setLoading(true);
 
-  console.log(tours);
- };
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      setLoading(false);
+      setTours(tours);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
+  // invoke fetchTours function
+  useEffect(() => {
+    fetchTours();
+  }, []);
 
- useEffect(() => {
-   fetchTours();
- })
- if(loading) {
-   return (
-   <main>
-     <Loading />
-   </main>
-   );
- }
+  // if loading true
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
   return (
-  <main>
-    <Tours />
-  </main>
- );
+    <main>
+      <Tours />
+    </main>
+  );
 }
 
-export default App
+export default App;

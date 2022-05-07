9045b94d078560fdbff4b69eff471d1e018e6e42
jubhavsar <juhi_bhavsar@yahoo.com>
2022-05-07 08:55:35 -0400

    define state and initialize them to set value

--- 02-tours/src/App.js
+++ 02-tours/src/App.js
@@ -5,6 +5,8 @@ import Tours from './Tours'
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-tours-project'
 function App() {
+ const [loading, setLoading] = useState(true);
+ const [tours, setTours] = useState([]);
   return <h2>Tours Project Setup</h2>
 }
 

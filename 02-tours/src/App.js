feb5129c67836ad81b1b9c735de8a37776f50073
jubhavsar <juhi_bhavsar@yahoo.com>
2022-05-07 09:13:36 -0400

    implement function to fetch data from declare Api

--- 02-tours/src/App.js
+++ 02-tours/src/App.js
@@ -5,20 +5,30 @@ import Tours from './Tours'
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-tours-project'
 function App() {
- const [loading, setLoading] = useState(false);
+ const [loading, setLoading] = useState(true);
  const [tours, setTours] = useState([]);
+
+ const fetchTours = async () => {
+  setLoading(true);
+  const response = await fetch(url);
+  const tours = await response.json();
+
+  console.log(tours);
+ };
+
+ 
  if(loading) {
    return (
    <main>
      <Loading />
    </main>
-   )
+   );
  }
   return (
   <main>
     <Tours />
   </main>
- )
+ );
 }
 
 export default App

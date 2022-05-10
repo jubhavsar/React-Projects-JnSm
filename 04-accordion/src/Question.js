744742156e4a0f280e8887154986c64375fb80f6
jubhavsar <juhi_bhavsar@yahoo.com>
2022-05-10 12:19:41 -0400

    implement toggle functionality using plus minus icons

--- 04-accordion/src/Question.js
+++ 04-accordion/src/Question.js
@@ -1,17 +1,20 @@
 import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
 const Question = ( { title, info } ) => {
+  const [showInfo, setShowInfo] = useState(false);
+
   return (
     <article className='question'>
       <header>
-        <h3>
+        <h4>
         {title}
-        </h3>
-        <button className='btn'>
-          btn
+        </h4>
+        <button className='btn' onClick={() => setShowInfo(!showInfo)}>
+          {showInfo ? <AiOutlineMinus /> : <AiOutlinePlus />}
         </button>
       </header>
-      <p>{info}</p>
+      {showInfo && <p>{info}</p>}
+      
     </article>
   );
 };

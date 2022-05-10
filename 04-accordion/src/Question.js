d9f1ba0843fd6681f3333f8980ea44edaee64ea9
jubhavsar <juhi_bhavsar@yahoo.com>
2022-05-10 11:52:44 -0400

    render questions data into Question component

--- 04-accordion/src/Question.js
+++ 04-accordion/src/Question.js
@@ -1,7 +1,19 @@
 import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+const Question = ( { title, info } ) => {
+  return (
+    <article className='question'>
+      <header>
+        <h3>
+        {title}
+        </h3>
+        <button className='btn'>
+          btn
+        </button>
+      </header>
+      <p>{info}</p>
+    </article>
+  );
 };
 
 export default Question;

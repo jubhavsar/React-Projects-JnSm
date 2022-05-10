d5f8d5790617af5d2a4a8830ea19a058e90592ae
jubhavsar <juhi_bhavsar@yahoo.com>
2022-05-10 09:14:04 -0400

    render img into Review component and style them

--- 03-Reviews/src/Review.js
+++ 03-Reviews/src/Review.js
@@ -4,9 +4,15 @@ import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
 
 const Review = () => {
   const [index, setIndex] = useState(0);
-  const {name, job, image, text} = people(index);
+  const {name, job, image, text} = people[index];
     
-  return <h2>review component</h2>;
+  return (
+    <article className='review'>
+      <div className='img-container'>
+        <img src={image} alt={name} className="person-img" />
+      </div>
+    </article>
+  );
 };
 
 export default Review;

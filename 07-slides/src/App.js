a0553d9449238c3ac427bfd8c345c014bdcba634
jubhavsar <juhi_bhavsar@yahoo.com>
2022-05-26 09:40:04 -0400

    define states

--- 07-slides/src/App.js
+++ 07-slides/src/App.js
@@ -3,6 +3,9 @@ import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
 import { FaQuoteRight } from 'react-icons/fa';
 import data from './data';
 function App() {
+  const [people, setPeople] = useState(data);
+  const [index, setIndex] = useState(0);
+
   return <h2>slider project setup</h2>;
 }
 

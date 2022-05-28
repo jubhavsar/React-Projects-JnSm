740e040f19acefa8d0d58b5fd398432d427c0ee0
jubhavsar <juhi_bhavsar@yahoo.com>
2022-05-28 14:30:10 -0400

    add # infront of hax values

--- 09-color-generator/src/SingleColor.js
+++ 09-color-generator/src/SingleColor.js
@@ -6,10 +6,11 @@ const SingleColor = ({ rgb, index, weight, hexColor }) => {
   const bcg = rgb.join(',');
   // to get hex color
   // const hex = rgbToHex(...rgb);
+  const hexValue = `#${hexColor}`;
   return (
     <article className={`color ${index > 10 && 'color-light'}`} style={{backgroundColor:`rgb(${bcg})` }}>
       <p className='percent-value'>{weight}%</p>
-      <p className='color-value'>{hexColor}</p>
+      <p className='color-value'>{hexValue}</p>
     </article>
   )
 }

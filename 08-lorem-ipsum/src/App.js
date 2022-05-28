42ed74c98c27510567befd743c1830592b0ae8c4
jubhavsar <juhi_bhavsar@yahoo.com>
2022-05-28 09:38:56 -0400

    check edge cases

--- 08-lorem-ipsum/src/App.js
+++ 08-lorem-ipsum/src/App.js
@@ -7,7 +7,12 @@ function App() {
   const handleSubmit = (e) => {
     e.preventDefault();
     let amount = parseInt(count);
-    console.log(amount);
+    if (count <= 0) {
+      amount = 1;
+    }
+    if (count > 8) {
+      amount = 8;
+    }
     setText(data.slice(0, amount));
   }
   return (

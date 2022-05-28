742bb0c750ddd099d4f368f32bf082684472465a
jubhavsar <juhi_bhavsar@yahoo.com>
2022-05-28 14:11:36 -0400

    convert rgb to hex color

--- 09-color-generator/src/SingleColor.js
+++ 09-color-generator/src/SingleColor.js
@@ -1,14 +1,14 @@
 import React, { useState, useEffect } from 'react'
 import rgbToHex from './utils'
 
-const SingleColor = ({rgb, index, weight, hex}) => {
+const SingleColor = ({rgb, index, weight, hexColor}) => {
   const [alert, setAlert] = useState(false);
   const bcg = rgb.join(',');
-  console.log(bcg);
+  const hex = rgbToHex(...rgb)
   return (
     <article className={`color`} style={{backgroundColor:`rgb(${bcg})` }}>
-      <p>{weight}</p>
-      <p>{hex}</p>
+      <p className='percent-value'>{weight}%</p>
+      <p className='color-value'>{hex}</p>
     </article>
   )
 }

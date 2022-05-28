fa937126fb826d4230e74c83ed76f9c9c364bcb1
jubhavsar <juhi_bhavsar@yahoo.com>
2022-05-28 09:09:01 -0400

    display form and onchange event

--- 08-lorem-ipsum/src/App.js
+++ 08-lorem-ipsum/src/App.js
@@ -4,9 +4,23 @@ function App() {
   const [count, setCount] = useState(0);
   const [text, setText] = useState([]);
   
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Hi");
+  }
   return (
   <section className='section-center'>
     <h3>Tired of boring lorem ipsome</h3>
+    <form className='lorem-form' onSubmit={handleSubmit}>
+      <label htmlFor="amount">paragraph:</label>
+      <input 
+        type="number" 
+        id='amount' 
+        name='amount' 
+        value={count} 
+        onChange={(e) => e.target.value}/>
+      <button className='btn' type='submit'>generate</button>
+    </form>
   </section>
     )
 }
